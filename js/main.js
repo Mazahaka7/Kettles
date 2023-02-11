51269eb503d7739d87325f1583a3944a8e90df65
git <dima.it.ua@gmail.com>
2023-02-11 19:15:29 +0200

    js done

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 //Submenu 
 const menuList = document.querySelector(".submenu");
 const btnSubMenu = document.getElementById("submenu");
+const main = document.querySelector("main");
     
 btnSubMenu.addEventListener("click", showSubMenu);
-    
+  
+//Submenu show & close function
 function showSubMenu() {
 menuList.classList.toggle("submenu--visible");
+}
+document.onclick = function (e) {
+    if (e.target.id !== "submenu" && e.target.className !== "submenu") {
+        menuList.classList.remove("submenu--visible");
+    }
 }
 
 //buttons
@@ -22,12 +29,13 @@ const kettleRedSmall = document.querySelector('.kettle--red');
 const kettlePinkSmall = document.querySelector('.kettle--pink');
 const kettleBegieSmall = document.querySelector('.kettle--biege');
 
-// Event listener for div with kettles img-s
+// Event listener for div with small kettles img-s
 kettleBlueSmall.addEventListener('click', colorBlue);
 kettleRedSmall.addEventListener('click', colorRed);
 kettlePinkSmall.addEventListener('click', colorPink);
 kettleBegieSmall.addEventListener('click', colorBegie);
 
+//switch-buttons functions
 btnColorBlue.addEventListener('click', colorBlue);
 function colorBlue() {
      if (colorPink || colorBegie || colorRed) {
@@ -36,6 +44,10 @@ function colorBlue() {
         mainPageColor.classList.remove('main--backgroundBegie');
         mainPageColor.classList.add('main--backgroundBlue');
      }
+    for (let i = 0; i < kettlesAll.length; i++) {
+        kettlesAll[i].classList.remove('kettle--background');
+    };
+    kettleBlueSmall.classList.add('kettle--background');
     document.getElementById("main__img").src = "./img/Smeg 1.png";
     //let kettleBackground = kettleBlueSmall.style.backgroundColor = 'white';
 } 
@@ -47,9 +59,13 @@ function colorRed() {
         mainPageColor.classList.remove('main--backgroundBegie')
         mainPageColor.classList.add('main--backgroundRed');
     }
+    for (let i = 0; i < kettlesAll.length; i++) {
+        kettlesAll[i].classList.remove('kettle--background');
+    };
+    kettleRedSmall.classList.add('kettle--background');
     document.getElementById("main__img").src = "./img/Smeg 2.png";
-   // let kettleBackground = kettleRedSmall.style.backgroundColor = 'white';
 }
+
 btnColorPink.addEventListener('click', colorPink);
 function colorPink() {
     if (colorBlue || colorBegie || colorRed) {
@@ -58,8 +74,11 @@ function colorPink() {
         mainPageColor.classList.remove('main--backgroundBegie')
         mainPageColor.classList.add('main--backgroundPink');
     }
+    for (let i = 0; i < kettlesAll.length; i++) {
+        kettlesAll[i].classList.remove('kettle--background');
+    };
+    kettlePinkSmall.classList.add('kettle--background');
     document.getElementById("main__img").src = "./img/Smeg 3.png";
-    //let kettleBackground = kettlePinkSmall.style.backgroundColor = 'white';
 } 
 btnColorBegie.addEventListener('click', colorBegie);
 function colorBegie() {
@@ -69,15 +88,20 @@ function colorBegie() {
         mainPageColor.classList.remove('main--backgroundPink');
         mainPageColor.classList.add('main--backgroundBegie');
     }
+    for (let i = 0; i < kettlesAll.length; i++) {
+        kettlesAll[i].classList.remove('kettle--background');
+    };
+    kettleBegieSmall.classList.add('kettle--background');
     document.getElementById("main__img").src = "./img/Smeg 4.png";
-    //let kettleBackground = kettleBegieSmall.style.backgroundColor = 'white';
 }
 
 for (let i = 0; i < kettlesAll.length; i++) {
-        kettlesAll[i].addEventListener('click', backgroundChangeColor);
-    }
-function backgroundChangeColor() {
+        kettlesAll[i].addEventListener('click', changeBackgroundColor);
+};
+
+function changeBackgroundColor() {
+    for (let i = 0; i < kettlesAll.length; i++) {
+        kettlesAll[i].classList.remove('kettle--background');
+};
     this.classList.add('kettle--background');
-    this.previousElementSibling.classList.remove('kettle--background');
-    this.nextElementSibling.classList.remove('kettle--background');
-}
+}   
